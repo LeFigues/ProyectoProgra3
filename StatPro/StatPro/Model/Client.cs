c6580aaebec556c3e4ac408bee2908976b1c5f75
Miguel Figueroa Perez <96083830+LeFigues@users.noreply.github.com>
2023-06-12 00:55:13 -0400

    First

---
+++ StatPro/StatPro/Model/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatPro.Model
+{
+    internal class Client
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string Cellphone { get; set; }
+
+
+
+
+    }
+
+}
---
+++ StatPro/StatPro/Model/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatPro.Model
+{
+    internal class Sale
+    {
+        public int Id { get; set; }
+        public int ClientID { get; set; }
+        public decimal Total { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
